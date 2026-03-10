a7a5cf5d9376796ed88a50d7187d8be0f1e32bb7
andriferdian24 <lanayascan01@gmail.com>
2026-03-10 16:31:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let number = Math.floor(Math.random()*10)+1;
 
+function checkGuess(){
+
+let guess = document.getElementById("guess").value;
+
+if(guess == number){
+document.getElementById("result").innerHTML = "🎉 Benar!";
+}
+else{
+document.getElementById("result").innerHTML = "❌ Salah, coba lagi!";
+}
+
+}
